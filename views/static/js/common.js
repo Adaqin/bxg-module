f10e9c0bb3e8eba3c7ad33c0434c23ae4a81b5e6
Clover <17602171930.@.163.com>
2017-07-03 18:31:14 +0800

    一些小的改动

--- views/static/js/common.js
+++ views/static/js/common.js
@@ -2,10 +2,10 @@ define(["jquery","template","cookie"],function($,template){
 	$(function(){
 		if(location.pathname!="/dashboard/login"){
 			var userInfo = JSON.parse($.cookie("userInfo"));
-			console.log(userInfo);
 			var html= template("profile-tpl",userInfo);
 			$('#userinfo').html(html);
 		}
 		
 	})
+	
 })
